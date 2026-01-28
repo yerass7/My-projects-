ea88e4231930908a98b47b0b160538ce04b84407
yerass7 <makhanbayerasyl@gmail.com>
2026-01-28 09:35:16 +0500

    Update vehicle.py

--- src/vehicle.py
+++ src/vehicle.py
@@ -1,1 +1,14 @@
+class Vehicle:
+    def __init__(self, brand, model, year):
+        self.brand = brand
+        self.model = model
+        self.year = year
 
+    def start_engine(self):
+        print(f"{self.brand} {self.model}: Engine started")
+
+    def stop_engine(self):
+        print(f"{self.brand} {self.model}: Engine stopped")
+
+    def __str__(self):
+        return f"{self.brand} {self.model} ({self.year})"
