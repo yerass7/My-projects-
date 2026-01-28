7d1a6e9a3de9e40a68d0ef37a652c9411c551a8d
yerass7 <makhanbayerasyl@gmail.com>
2026-01-28 09:36:17 +0500

    Update garage.py

--- src/garage.py
+++ src/garage.py
@@ -1,1 +1,18 @@
+class Garage:
+    def __init__(self, name):
+        self.name = name
+        self.vehicles = []
 
+    def add_vehicle(self, vehicle):
+        self.vehicles.append(vehicle)
+        print(f"{vehicle} added to garage '{self.name}'")
+
+    def remove_vehicle(self, vehicle):
+        if vehicle in self.vehicles:
+            self.vehicles.remove(vehicle)
+            print(f"{vehicle} removed from garage '{self.name}'")
+
+    def list_vehicles(self):
+        print(f"\nGarage: {self.name}")
+        for vehicle in self.vehicles:
+            print("-", vehicle)
