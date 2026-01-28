b88ab62b9bceff244d4882f9f90ca4c47f255e20
yerass7 <makhanbayerasyl@gmail.com>
2026-01-28 09:35:37 +0500

    Update car.py

--- src/car.py
+++ src/car.py
@@ -1,1 +1,11 @@
+from src.vehicle import Vehicle
+
+class Car(Vehicle):
+    def __init__(self, brand, model, year, doors, transmission):
+        super().__init__(brand, model, year)
+        self.doors = doors
+        self.transmission = transmission
+
+    def start_engine(self):
+        print(f"Car {self.brand} {self.model}: Engine started automatically")
 
